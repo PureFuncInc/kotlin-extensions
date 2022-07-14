49b6af39e210dc993d76f4af394e7cf19004bf1e
Vincent Huang iMac <yfr.huang@hotmail.com>
2022-07-15 00:08:12 +0800

    :bug: fix: aes bug

--- src/main/kotlin/net/purefunc/kotlin/ext/Aes.kt
+++ src/main/kotlin/net/purefunc/kotlin/ext/Aes.kt
@@ -1,55 +1,40 @@
 package net.purefunc.kotlin.ext
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.nio.charset.StandardCharsets
 import java.security.Security
 import java.util.Base64
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
-sealed class Transformation(
-    val value: String,
-) {
-    object AES_CBC_PKCS7PADDING : Transformation("AES/CBC/PKCS7PADDING")
-}
-
-data class AesKeyPair(
-    private val key: String,
-    private val iv: String,
-) {
-    companion object {
-        fun create(key: String, iv: String) =
-            apply {
-                if (key.length != 16 || iv.length != 16) throw RuntimeException()
-            }.run {
-                AesKeyPair(key, iv)
-            }
-    }
-
-    fun getKeySpec() = SecretKeySpec(key.toByteArray(), "AES")
-    fun getIvSpec() = IvParameterSpec(key.toByteArray())
-}
-
 fun String.aesEncrypt(
-    transformation: Transformation = Transformation.AES_CBC_PKCS7PADDING,
-    aesKeyPair: AesKeyPair,
-): String = this.aes(transformation = transformation, mode = Cipher.ENCRYPT_MODE, aesKeyPair = aesKeyPair)
+    transformation: String = "AES/CBC/PKCS7PADDING",
+    aesKey: String,
+    aesIv: String,
+): String = this.aes(transformation = transformation, mode = Cipher.ENCRYPT_MODE, aesKey, aesIv)
 
 fun String.aesDecrypt(
-    transformation: Transformation = Transformation.AES_CBC_PKCS7PADDING,
-    aesKeyPair: AesKeyPair,
-): String = this.aes(transformation = transformation, mode = Cipher.DECRYPT_MODE, aesKeyPair = aesKeyPair)
+    transformation: String = "AES/CBC/PKCS7PADDING",
+    aesKey: String,
+    aesIv: String,
+): String = this.aes(transformation = transformation, mode = Cipher.DECRYPT_MODE, aesKey, aesIv)
 
 private fun String.aes(
-    transformation: Transformation,
+    transformation: String,
     mode: Int,
-    aesKeyPair: AesKeyPair,
+    aesKey: String,
+    aesIv: String,
 ): String = also {
     Security.addProvider(BouncyCastleProvider())
 }.let {
-    Cipher.getInstance(transformation.value)
+    Cipher.getInstance(transformation)
 }.also {
-    it.init(mode, aesKeyPair.getKeySpec(), aesKeyPair.getIvSpec())
+    it.init(
+        mode,
+        SecretKeySpec(aesKey.toByteArray(StandardCharsets.UTF_8), "AES"),
+        IvParameterSpec(aesIv.toByteArray(StandardCharsets.UTF_8))
+    )
 }.let {
     if (mode == Cipher.ENCRYPT_MODE) String(Base64.getEncoder().encode(it.doFinal(this.toByteArray())))
     else String(it.doFinal(Base64.getDecoder().decode(this)))
--- src/test/kotlin/net/purefunc/kotlin/ext/AesTests.kt
+++ src/test/kotlin/net/purefunc/kotlin/ext/AesTests.kt
@@ -8,21 +8,11 @@ class AesTests {
     @Test
     internal fun `test aes encrypt & decrypt`() {
         val rawString = "Hello World"
-        val rawString2 = "Hello World2"
 
-        val transformation = Transformation.AES_CBC_PKCS7PADDING
-        val aesKeyPair = AesKeyPair.create("1234567890123456", "0123456789123456")
-
-        val result = "Hello World"
-            .aesEncrypt(transformation, aesKeyPair)
-            .aesDecrypt(transformation, aesKeyPair)
+        val result = rawString
+            .aesEncrypt(aesKey = "1234567890123456", aesIv = "0123456789123456")
+            .aesDecrypt(aesKey = "1234567890123456", aesIv = "0123456789123456")
 
         Assertions.assertEquals(rawString, result)
-
-        val result2 = "Hello World2"
-            .aesEncrypt(aesKeyPair = aesKeyPair)
-            .aesDecrypt(aesKeyPair = aesKeyPair)
-
-        Assertions.assertEquals(rawString2, result2)
     }
 }

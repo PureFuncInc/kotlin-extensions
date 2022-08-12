b3abc8bbf2986405954b051f01e72c1babe295be
黃健旻 <yfr.huang@hotmail.com>
2022-08-12 23:27:04 +0800

    :sparkles: feat: refine zipAllEither (#10)

--- src/main/kotlin/net/purefunc/kotlin/ext/ArrowKt.kt
+++ src/main/kotlin/net/purefunc/kotlin/ext/ArrowKt.kt
@@ -17,14 +17,19 @@ open class AppErr(
     val message: String,
 )
 
-fun <L : AppErr, R> R?.catchErrWhenNull(
+suspend fun <L : AppErr, R> R?.catchErrWhenNull(
     appErr: L,
-    block: () -> Unit = {},
+    block: suspend () -> Unit,
 ): Either<L, R> =
     toOption()
         .fold(
-            ifEmpty = { appErr.left() },
-            ifSome = { it.right() },
+            ifEmpty = {
+                appErr.left()
+            },
+            ifSome = {
+                block.invoke()
+                it.right()
+            },
         )
 
 suspend fun <L : AppErr, R> R.catchErrWhenTrue(
@@ -41,10 +46,10 @@ suspend fun <L : AppErr, R> R.catchErrWhenFalse(
     if (!block.invoke(this)) appErr.left()
     else right()
 
-suspend inline fun <L : AppErr, reified R, T> R.catchErrWhenApply(
+inline fun <L : AppErr, reified R, T> R.catchErrWhenApply(
     appErr: L,
     printTrace: Boolean = false,
-    block: suspend (R) -> T,
+    block: (R) -> T,
 ): Either<L, R> =
     try {
         block.invoke(this)
@@ -54,10 +59,10 @@ suspend inline fun <L : AppErr, reified R, T> R.catchErrWhenApply(
         appErr.left()
     }
 
-suspend inline fun <L : AppErr, reified R, T> R.catchErrWhenRun(
+inline fun <L : AppErr, reified R, T> R.catchErrWhenRun(
     appErr: L,
     printTrace: Boolean = false,
-    block: suspend (R) -> T,
+    block: (R) -> T,
 ): Either<L, T> =
     try {
         block.invoke(this).right()
@@ -66,12 +71,12 @@ suspend inline fun <L : AppErr, reified R, T> R.catchErrWhenRun(
         appErr.left()
     }
 
-fun <L : AppErr, R> Either<L, R?>.flatCatchErrWhenNull(
+suspend fun <L : AppErr, R> Either<L, R?>.flatCatchErrWhenNull(
     appErr: L,
-    block: () -> Unit = {},
+    block: suspend () -> Unit,
 ): Either<L, R> =
     flatMap {
-        it.catchErrWhenNull(appErr)
+        it.catchErrWhenNull(appErr, block)
     }
 
 suspend fun <L : AppErr, R> Either<L, R>.flatCatchErrWhenTrue(
@@ -90,7 +95,7 @@ suspend fun <L : AppErr, R> Either<L, R>.flatCatchErrWhenFalse(
         it.catchErrWhenFalse(appErr, block)
     }
 
-suspend inline fun <L : AppErr, reified R, T> Either<L, R>.flatCatchErrWhenApply(
+inline fun <L : AppErr, reified R, T> Either<L, R>.flatCatchErrWhenApply(
     appErr: L,
     printTrace: Boolean = false,
     block: (R) -> T,
@@ -99,7 +104,7 @@ suspend inline fun <L : AppErr, reified R, T> Either<L, R>.flatCatchErrWhenApply
         it.catchErrWhenApply(appErr, printTrace, block)
     }
 
-suspend inline fun <L : AppErr, reified R, T> Either<L, R>.flatCatchErrWhenRun(
+inline fun <L : AppErr, reified R, T> Either<L, R>.flatCatchErrWhenRun(
     appErr: L,
     printTrace: Boolean = false,
     block: (R) -> T,
@@ -115,16 +120,20 @@ fun <L : AppErr> zipAllEither(
         .toList()
         .traverse { it }
 
-fun <L : AppErr, R> R?.validErrWhenNull(
+suspend fun <L : AppErr, R> R?.validErrWhenNull(
     appErr: L,
-    block: () -> Unit = {},
+    block: suspend () -> Unit,
 ): ValidatedNel<L, R> =
     toOption()
         .fold(
-            ifEmpty = { appErr.invalid() },
-            ifSome = { it.valid() },
-        )
-        .toValidatedNel()
+            ifEmpty = {
+                appErr.invalid()
+            },
+            ifSome = {
+                block.invoke()
+                it.valid()
+            },
+        ).toValidatedNel()
 
 suspend fun <L : AppErr, R> R.validErrWhenTrue(
     appErr: L,
@@ -138,10 +147,10 @@ suspend fun <L : AppErr, R> R.validErrWhenFalse(
 ): ValidatedNel<L, R> =
     (if (!block.invoke(this)) appErr.invalid() else valid()).toValidatedNel()
 
-suspend inline fun <L : AppErr, reified R, T> R.validErrWhenApply(
+inline fun <L : AppErr, reified R, T> R.validErrWhenApply(
     appErr: L,
     printTrace: Boolean = false,
-    block: suspend (R) -> T,
+    block: (R) -> T,
 ): ValidatedNel<L, R> =
     try {
         block.invoke(this)
@@ -151,10 +160,10 @@ suspend inline fun <L : AppErr, reified R, T> R.validErrWhenApply(
         appErr.invalid()
     }.toValidatedNel()
 
-suspend inline fun <L : AppErr, reified R, T> R.validErrWhenRun(
+inline fun <L : AppErr, reified R, T> R.validErrWhenRun(
     appErr: L,
     printTrace: Boolean = false,
-    block: suspend (R) -> T,
+    block: (R) -> T,
 ): ValidatedNel<L, T> =
     try {
         block.invoke(this).valid()
@@ -165,12 +174,12 @@ suspend inline fun <L : AppErr, reified R, T> R.validErrWhenRun(
 
 typealias EitherNel<A, B> = Either<Nel<A>, B>
 
-fun <L : AppErr, R> EitherNel<L, R?>.flatValidErrWhenNull(
+suspend fun <L : AppErr, R> EitherNel<L, R?>.flatValidErrWhenNull(
     appErr: L,
-    block: () -> Unit = {},
+    block: suspend () -> Unit,
 ): EitherNel<L, R> =
     flatMap {
-        it.validErrWhenNull(appErr).toEither()
+        it.validErrWhenNull(appErr, block).toEither()
     }
 
 suspend fun <L : AppErr, R> EitherNel<L, R>.flatValidErrWhenTrue(
@@ -189,7 +198,7 @@ suspend fun <L : AppErr, R> EitherNel<L, R>.flatValidErrWhenFalse(
         it.validErrWhenFalse(appErr, block).toEither()
     }
 
-suspend inline fun <L : AppErr, reified R, T> EitherNel<L, R>.flatValidErrWhenApply(
+inline fun <L : AppErr, reified R, T> EitherNel<L, R>.flatValidErrWhenApply(
     appErr: L,
     printTrace: Boolean = false,
     block: (R) -> T,
@@ -198,7 +207,7 @@ suspend inline fun <L : AppErr, reified R, T> EitherNel<L, R>.flatValidErrWhenAp
         it.validErrWhenApply(appErr, printTrace, block).toEither()
     }
 
-suspend inline fun <L : AppErr, reified R, T> EitherNel<L, R>.flatValidErrWhenRun(
+inline fun <L : AppErr, reified R, T> EitherNel<L, R>.flatValidErrWhenRun(
     appErr: L,
     printTrace: Boolean = false,
     block: (R) -> T,
@@ -208,7 +217,7 @@ suspend inline fun <L : AppErr, reified R, T> EitherNel<L, R>.flatValidErrWhenRu
     }
 
 fun <L : AppErr> zipAllValid(
-    vararg validateNels: ValidatedNel<L, *>
+    vararg validateNels: ValidatedNel<L, *>,
 ): EitherNel<L, List<*>> =
     validateNels
         .toList()

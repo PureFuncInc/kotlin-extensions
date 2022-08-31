67b4c25da0398e2bfff0b1a936f57372132cbef8
黃健旻 <yfr.huang@hotmail.com>
2022-08-31 19:24:27 +0800

    :bug: fix: update domain repository (#19)

--- src/main/kotlin/net/purefunc/kotlin/ddd/domain/repository/DomainRepository.kt
+++ src/main/kotlin/net/purefunc/kotlin/ddd/domain/repository/DomainRepository.kt
@@ -1,9 +1,10 @@
 package net.purefunc.kotlin.ddd.domain.repository
 
+import arrow.core.Either
 import net.purefunc.kotlin.ddd.domain.entity.DomainAggregateRoot
 import net.purefunc.kotlin.ddd.domain.entity.DomainEntityId
+import net.purefunc.kotlin.ext.AppErr
 
 interface DomainRepository<ID : DomainEntityId, R : DomainAggregateRoot<ID>> {
-
-    suspend fun findByEntityId(entityId: ID): R?
+    suspend fun findByEntityId(entityId: ID): Either<AppErr, R?>
 }

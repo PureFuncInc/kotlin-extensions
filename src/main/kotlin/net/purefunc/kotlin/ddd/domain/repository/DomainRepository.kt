ebec706d4313f2a8d0546ff1be526ea2c80858a7
黃健旻 <yfr.huang@hotmail.com>
2022-09-02 12:38:06 +0800

    :sparkles: refactor: change AppErr to E: AppErr (#23)

--- src/main/kotlin/net/purefunc/kotlin/ddd/domain/repository/DomainRepository.kt
+++ src/main/kotlin/net/purefunc/kotlin/ddd/domain/repository/DomainRepository.kt
@@ -5,17 +5,17 @@ import net.purefunc.kotlin.ddd.domain.entity.DomainAggregateRoot
 import net.purefunc.kotlin.ddd.domain.entity.DomainEntityId
 import net.purefunc.kotlin.ext.AppErr
 
-interface DomainRepository<ID : DomainEntityId, R : DomainAggregateRoot<ID>> {
+interface DomainRepository<ID : DomainEntityId, R : DomainAggregateRoot<ID>, E : AppErr> {
 
-    suspend fun save(entity: R): Either<AppErr, R>
+    suspend fun save(entity: R): Either<E, R>
 
-    suspend fun findById(id: ID): Either<AppErr, R?>
+    suspend fun findById(id: ID): Either<E, R?>
 
-    suspend fun existsById(id: ID): Either<AppErr, Boolean>
+    suspend fun existsById(id: ID): Either<E, Boolean>
 
-    suspend fun count(): Either<AppErr, Long>
+    suspend fun count(): Either<E, Long>
 
-    suspend fun deleteById(id: ID): Either<AppErr, Unit>
+    suspend fun deleteById(id: ID): Either<E, Unit>
 
-    suspend fun delete(entity: R): Either<AppErr, Unit>
+    suspend fun delete(entity: R): Either<E, Unit>
 }

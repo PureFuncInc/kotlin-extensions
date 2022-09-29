b0295f4a315babe374cc7b2ebdce0514a86b5389
Vincent Huang MacBook <yfr.huang@hotmail.com>
2022-09-29 11:10:33 +0800

    :sparkles: feat: refactor CommandResponse.kt

--- src/main/kotlin/net/purefunc/kotlin/ddd/interfaces/CommandResponse.kt
+++ src/main/kotlin/net/purefunc/kotlin/ddd/interfaces/CommandResponse.kt
@@ -1,18 +1,17 @@
 package net.purefunc.kotlin.ddd.interfaces
 
-class CommandResponse<T> private constructor(
-    var status: Int?,
-    var headers: Map<String, String>?,
-    var body: T?,
+data class CommandResponse<T>(
+    val status: Int,
+    val headers: Map<String, String>,
+    val body: T,
 ) {
 
-    data class Builder<T>(
-        var status: Int?,
-        var headers: Map<String, String>?,
-        var body: T?,
-    ) {
-        fun status(status: Int): Builder<T> = apply { this.status = status }
-        fun headers(headers: Map<String, String>): Builder<T> = apply { this.headers = headers }
-        fun body(t: T): CommandResponse<T> = CommandResponse(status, headers, body)
-    }
+    fun addHeader(key: String, value: String): CommandResponse<T> =
+        copy(
+            headers = headers
+                .toMutableMap()
+                .apply {
+                    put(key, value)
+                }
+        )
 }
